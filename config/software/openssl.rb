5fb869c99a0820b59094b83e6cbaaefdb2093f63
johnwilliams <john@basecamp.com>
2021-01-05 14:06:36 -0600

    Update openssl.rb

--- config/software/openssl.rb
+++ config/software/openssl.rb
@@ -27,7 +27,7 @@ source url: "https://www.openssl.org/source/openssl-#{version}.tar.gz"
 
 version("1.0.1m") { source md5: "d143d1555d842a069cb7cc34ba745a06" }
 version("1.0.1p") { source md5: "7563e92327199e0067ccd0f79f436976" }
-version("1.0.2x") { source sha256: "79cb4e20004a0d1301210aee7e154ddfba3d6a33d0df1f6c5d3257cb915a59c9" }
+version("1.0.2u") { source sha256: "ecd0c6ffb493dd06707d38b14bb4d8c2288bb7033735606569d8f90f89669d16" }
 
 relative_path "openssl-#{version}"
 
