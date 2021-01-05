5fcb4bd1b8e8ea8157f97eb14fdae1e8d7286047
johnwilliams <john@basecamp.com>
2021-01-05 16:42:52 -0600

    Revert chef.

--- config/software/chef.rb
+++ config/software/chef.rb
@@ -1,5 +1,5 @@
 #
-# Copyright:: Copyright (c) Chef Software Inc.
+# Copyright 2012-2014 Chef Software, Inc.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -16,65 +16,65 @@
 name "chef"
 default_version "12.5.1"
 
-license "Apache-2.0"
-license_file "LICENSE"
-
-# Grab accompanying notice file.
-# So that Open4/deep_merge/diff-lcs disclaimers are present in Omnibus LICENSES tree.
-license_file "NOTICE"
-
-# For the specific super-special version "local_source", build the source from
-# the local git checkout. This is what you'd want to occur by default if you
-# just ran omnibus build locally.
-version("local_source") do
-  source path: "#{project.files_path}/../..",
-         # Since we are using the local repo, we try to not copy any files
-         # that are generated in the process of bundle installing omnibus.
-         # If the install steps are well-behaved, this should not matter
-         # since we only perform bundle and gem installs from the
-         # omnibus cache source directory, but we do this regardless
-         # to maintain consistency between what a local build sees and
-         # what a github based build will see.
-         options: { exclude: [ "omnibus/vendor" ] }
-end
-
-# For any version other than "local_source", fetch from github.
-# This is the behavior the transitive omnibus software deps such as chef-dk
-# expect.
-if version != "local_source"
-  source git: "https://github.com/chef/chef.git"
-end
+source git: "git://github.com/chef/chef"
 
 relative_path "chef"
 
-dependency "ruby"
+if windows?
+  dependency "ruby-windows"
+  dependency "openssl-windows"
+  dependency "ruby-windows-devkit"
+  dependency "ruby-windows-devkit-bash"
+  dependency "cacerts"
+else
+  dependency "ruby"
+  dependency "libffi"
+end
+
+dependency "rubygems"
+dependency "bundler"
 dependency "ohai"
-dependency "libarchive" # for archive resource
+dependency "appbundler"
 
 build do
   env = with_standard_compiler_flags(with_embedded_path)
 
-  # The --without groups here MUST match groups in https://github.com/chef/chef/blob/master/Gemfile
-  excluded_groups = %w{docgen chefstyle}
-  excluded_groups << "ruby_prof" if aix?
-  excluded_groups << "ruby_shadow" if aix?
-  excluded_groups << "ed25519" if solaris2?
+  if windows?
+    # Normally we would symlink the required unix tools.
+    # However with the introduction of git-cache to speed up omnibus builds,
+    # we can't do that anymore since git on windows doesn't support symlinks.
+    # https://groups.google.com/forum/#!topic/msysgit/arTTH5GmHRk
+    # Therefore we copy the tools to the necessary places.
+    # We need tar for 'knife cookbook site install' to function correctly
+    {
+      'tar.exe'          => 'bsdtar.exe',
+      'libarchive-2.dll' => 'libarchive-2.dll',
+      'libexpat-1.dll'   => 'libexpat-1.dll',
+      'liblzma-1.dll'    => 'liblzma-1.dll',
+      'libbz2-2.dll'     => 'libbz2-2.dll',
+      'libz-1.dll'       => 'libz-1.dll',
+    }.each do |target, to|
+      copy "#{install_dir}/embedded/mingw/bin/#{to}", "#{install_dir}/bin/#{target}"
+    end
+  end
 
-  bundle "install --without #{excluded_groups.join(" ")}", env: env
+  # install the whole bundle first
+  bundle "install --without server docgen", env: env
 
-  # use the rake install task to build/install chef-config/chef-utils
-  bundle "exec rake install", env: env
+  # Install components that live inside Chef's git repo. For now this is just
+  # 'chef-config'
+  bundle "exec rake install_components", env: env
 
-  gemspec_name = windows? ? "chef-universal-mingw32.gemspec" : "chef.gemspec"
+  gemspec_name = windows? ? 'chef-windows.gemspec' : 'chef.gemspec'
 
   # This step will build native components as needed - the event log dll is
   # generated as part of this step.  This is why we need devkit.
   gem "build #{gemspec_name}", env: env
 
-  # ensure we put the gems in the right place to get picked up by the publish scripts
-  delete "pkg"
-  mkdir "pkg"
-  copy "chef*.gem", "pkg"
+  # Don't use -n #{install_dir}/bin. Appbundler will take care of them later
+  gem "install chef*.gem " \
+      " --no-ri --no-rdoc" \
+      " --verbose", env: env
 
   # Always deploy the powershell modules in the correct place.
   if windows?
@@ -82,20 +82,25 @@ build do
     copy "distro/powershell/chef/*", "#{install_dir}/modules/chef"
   end
 
-  block do
-    appbundle "chef", lockdir: project_dir, gem: "inspec-core-bin", without: excluded_groups, env: env
-    appbundle "chef", lockdir: project_dir, gem: "chef-bin", without: excluded_groups, env: env
-    appbundle "chef", lockdir: project_dir, gem: "chef", without: excluded_groups, env: env
-    appbundle "chef", lockdir: project_dir, gem: "ohai", without: excluded_groups, env: env
+  auxiliary_gems = {}
+  auxiliary_gems['ruby-shadow'] = '>= 0.0.0' unless aix? || windows?
+
+  auxiliary_gems.each do |name, version|
+    gem "install #{name}" \
+        " --version '#{version}'" \
+        " --no-ri --no-rdoc" \
+        " --verbose", env: env
   end
 
-  # The rubyzip gem ships with some test fixture data compressed in a format Apple's notarization service
-  # cannot understand. We need to delete that archive to pass notarization.
-  block "Delete test folder of rubyzip gem so downstream projects pass notarization" do
-    env["VISUAL"] = "echo"
-    %w{rubyzip}.each do |gem|
-      gem_install_dir = shellout!("#{install_dir}/embedded/bin/gem open #{gem}", env: env).stdout.chomp
-      remove_directory "#{gem_install_dir}/test"
-    end
-  end
+  appbundle 'chef'
+  appbundle 'ohai'
+
+  # Clean up
+  delete "#{install_dir}/embedded/docs"
+  delete "#{install_dir}/embedded/share/man"
+  delete "#{install_dir}/embedded/share/doc"
+  delete "#{install_dir}/embedded/share/gtk-doc"
+  delete "#{install_dir}/embedded/ssl/man"
+  delete "#{install_dir}/embedded/man"
+  delete "#{install_dir}/embedded/info"
 end
